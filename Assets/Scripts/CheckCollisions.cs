72c3a1752292b6a70162f0da48b27610ee90a0bf
SelinAlagoz <76443564+SelinAlagoz@users.noreply.github.com>
2023-06-13 15:29:49 +0300

    added collider obstacle

--- Assets/Scripts/CheckCollisions.cs
+++ Assets/Scripts/CheckCollisions.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; //for reachng to text mesh pro 
 
 public class CheckCollisions : MonoBehaviour
 {
+
+  public int score;
+  public TextMeshProUGUI BookText;
 private void OnTriggerEnter(Collider other)
 {
  if(other.CompareTag("Book"))
  {
-   Destroy(other.gameObject);
+   AddBook();
+   other.gameObject.SetActive(false); 
+    //Destroy(other.gameObject);
     //Debug.Log(other.gameObject.name);
  }
+}
+public void AddBook()
+{
+  score++;
+  BookText.text = "Score: " + score.ToString();
 }
 }
